04c4e7c49e9255805ece8e3bca06274b26578ebd
amanagarwal <amanagarwal82710@gmail.com>
2024-11-19 22:15:07 +0530

    dsa-19th nov

---
+++ StackQueues/NextGreaterElement2.java
@@ -0,0 +1,23 @@
+package leetcodeProblems.StackQueues;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class NextGreaterElement2 {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[n];
+        Arrays.fill(ans,-1);
+        for(int i=(2*n)-1; i>=0; i--){
+            while (!st.isEmpty() && nums[i%n]> st.peek()){
+                st.pop();
+            }
+            if(!st.isEmpty() && i<n){
+                ans[i] = st.peek();
+            }
+            st.push(nums[i%n]);
+        }
+        return ans;
+    }
+}
